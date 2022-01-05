bb4b3d6858164585c071aa56f78878fb504e8941
Jean-Poul <cph-jl360@cphbusiness.dk>
2022-01-05 09:40:50 +0100

    minor fix

--- src/main/java/errorhandling/NoConnectionException.java
+++ src/main/java/errorhandling/NoConnectionException.java
@@ -3,8 +3,6 @@ package errorhandling;
 // Homemade exception
 public class NoConnectionException extends Exception {
 
-    private String message;
-
     // Constructors
     public NoConnectionException(String msg) {
         super(msg);
